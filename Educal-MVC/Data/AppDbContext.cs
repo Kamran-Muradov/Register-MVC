8ab615073e176b148aabf185a1f7b793540304b1
Kamran-Muradov <kamranshmu@code.edu.az>
2024-06-04 00:11:41 +0400

    Register task

---
+++ Educal-MVC/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Educal_MVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Educal_MVC.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseImage> CourseImages { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Course>().HasQueryFilter(m => !m.SoftDeleted);
+            builder.Entity<Category>().HasQueryFilter(m => !m.SoftDeleted);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
